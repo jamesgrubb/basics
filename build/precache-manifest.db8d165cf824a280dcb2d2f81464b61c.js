ba7d393497e255a039d064fd2b140e4fae4e1a96
jamesgrubb <jamesgrubb@mac.com>
2020-01-15 19:32:17 +0000

    build

---
+++ build/precache-manifest.db8d165cf824a280dcb2d2f81464b61c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "942c2d276b43b312b1566606bc9203c7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a9b62ce33a322664dcad",
+    "url": "/static/css/main.2badd065.chunk.css"
+  },
+  {
+    "revision": "32935c41d3b6ed0b390a",
+    "url": "/static/js/2.ab334172.chunk.js"
+  },
+  {
+    "revision": "a4238d9b9f29bfeea1cc4e1b5f7ba331",
+    "url": "/static/js/2.ab334172.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a9b62ce33a322664dcad",
+    "url": "/static/js/main.7a68a07d.chunk.js"
+  },
+  {
+    "revision": "00db40f7f80411ee77d9",
+    "url": "/static/js/runtime-main.7d5008bd.js"
+  }
+]);
