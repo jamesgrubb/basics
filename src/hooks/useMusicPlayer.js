236987884244be36894cc22f1cb458d9d1d0fbfa
jamesgrubb <jamesgrubb@mac.com>
2020-01-15 12:24:11 +0000

    Tweaks

---
+++ src/hooks/useMusicPlayer.js
@@ -0,0 +1,12 @@
+import {useContext} from 'react'
+import {MusicPlayerContext} from '../MusicPlayerContext'
+
+const useMusicPlayer = () => {
+    const [state,setState] = useContext(MusicPlayerContext)
+
+    
+
+    return (  );
+}
+ 
+export default useMusicPlayer;
